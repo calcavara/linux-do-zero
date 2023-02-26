a1c83ca22fe4e0cbbcf5613506d0867318509a05
calcavara <gcalcavara@gmail.com>
2023-02-26 12:53:06 -0300

    Arquivo de configuração de servidor Apache para o segundo projeto do bootcamp Linux do Zero

---
+++ apache-server-config.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Script para configuração de servidor web Apache2
+
+echo 'Iniciando configuração...'
+
+#Atualiza repositórios e aplicações da máquina
+echo 'Atualizando repositórios...'
+apt update -y
+echo 'Instalando atualizações disponíveis para aplicações...'
+apt upgrade -y
+
+#Instala o servidor apache2
+echo 'Instalando o servidor Apache2...'
+apt install apache2
+echo 'Habilitando o serviço Apache2 para iniciar junto com o servidor...'
+systemctl enable apache2
+echo 'Garantindo que o serviço do apache2 está no rodando...'
+systemctl start apache2
+
+#Instala o unzip
+echo 'Instalando descompactador de arquivos unzip...'
+apt install unzip
+
+#Baixa a aplicação em repositório do GitHub
+echo 'Baixando aplicação do Github...'
+wget -P /tmp https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip --show-progress
+
+#Descompacta a aplicação
+echo 'Descompactando a aplicação...'
+unzip /tmp/main.zip
+
+#Apaga arquivos na raíz do Apache
+echo 'Limpando diretório raíz do Apache'
+rm -rf /var/www/html/*
+
+#Copia a aplicação para a pasta raíz do Apache
+echo 'Copiando a aplicação para a pasta raíz do Apache'
+cp -r -v /tmp /var/www/html
+
+echo 'Instalação da aplicação concluída!!'
